4a18326444b8ba63c74aaa821d3971f5f6a87b93
cairns <lazycairx@gmail.com>
2015-03-21 09:40:26 +0100

    Add inline comments to timer.js.

--- timer.js
+++ timer.js
@@ -1,23 +1,53 @@
+
 function Timer() {}
+/* ------------------------------------------------- *
+ * Simple timer.
+ * @section Timer
+ * ------------------------------------------------- */
 Timer.prototype = (function () {
     'use strict';
 
     var self = {};
     var started = null, stopped = null;
 
+
+    /* ------------------------------------------------- *
+     * Start timer.
+     * ------------------------------------------------- */
     self.start = function() {
         started = Date.now();
         stopped = null;
     };
+
+
+    /* ------------------------------------------------- *
+     * Stop timer.
+     * ------------------------------------------------- */
     self.stop = function() {
         if (started) {
             stopped = Date.now();
         }
     };
+
+
+    /* ------------------------------------------------- *
+     * Reset timer.
+     * ------------------------------------------------- */
     self.reset = function() {
         started = null;
         stopped = null;
     };
+
+    /* ------------------------------------------------- *
+     * Returns the number of milliseconds that has
+     * elapsed between start and stop was called
+     * respectively. If the timer is running, the
+     * currently elapsed time will be returned.
+     * If the timer has not been started '0' will be
+     * the value.
+     *
+     * @return {Integer} Elapsed milliseconds.
+     * ------------------------------------------------- */
     self.time = function() {
         if (started && stopped) {
             return stopped - started;
@@ -27,6 +57,11 @@ Timer.prototype = (function () {
             return 0;
         }
     };
+
+
+    /* ------------------------------------------------- *
+     * @return {Boolean} True while the timer is active.
+     * ------------------------------------------------- */
     self.running = function() {
         return (started !== null && stopped === null);
     };
